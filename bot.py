ede1409d06791dd629717c85feb4ddf41a806bc9
centenocolqueg <centenocolqueg@gmail.com>
2026-08-03 20:58:53 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from aiogram.types import (
     InlineKeyboardMarkup,
-    InlineKeyboardButton, 
+    InlineKeyboardButton,
     FSInputFile
 )
 
